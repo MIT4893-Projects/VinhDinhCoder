0dd719d3ac41517b24e017c7ca985d83b17d4a1c
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-05-09 13:13:53 +0700

    1. TONG - Tổng A + B

---
+++ 1_TONG.cpp
@@ -0,0 +1,13 @@
+//* 756102	01/05/2023 18:31:29	Minh4893IT	TONG - Tổng A + B	GNU C++11	Accepted	15 ms	1036 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+using sint = signed int;
+#define int long long int
+
+sint main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+}
