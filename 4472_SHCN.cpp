2b47f5d0e0f7cb9b5803c65d22af13bc2ace47a8
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-05-09 13:12:55 +0700

    4472. SHCN - Diện tích HCN

---
+++ 4472_SHCN.cpp
@@ -0,0 +1,13 @@
+//* 756106	01/05/2023 18:32:57	Minh4893IT	SHCN - Diện tích HCN	GNU C++11	Accepted	15 ms	1092 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+using sint = signed int;
+#define int long long int
+
+sint main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a * b;
+}
