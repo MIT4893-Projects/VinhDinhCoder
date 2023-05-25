dfe84665683a656f4775699a975e27eef8d16f35
MIT4893-Projects <116936560+MIT4893-Projects@users.noreply.github.com>
2023-05-25 12:57:24 +0700

    Update .template.cpp

--- .template.cpp
+++ .template.cpp
@@ -1,49 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using sint = signed int;
-#define ll long long
-#define ull unsigned ll
-#define int ll
+#define int long long
 #define ss stringstream
 #define uset unordered_set
 #define umap unordered_map
-#define vec vector
+#define mmap multimap
+#define mset multiset
 #define pq priority_queue
+#define endl "\n"
 
 #define LLMAX INT64_MAX
-
-#define in cin
-#define out cout
-#define input in
-#define print out
 #define GET(var) getline(cin, var)
-
-#define endl "\n"
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-
-#define FORIR(i, a, n) for (int i = a; i < n; i++)
-#define FORI(i, n) FORIR(i, 0, n)
-#define FORR(a, n) FORIR(i, a, n)
-#define FOR(n) FORIR(i, 0, n)
 #define EACH(x, a) for (auto &x : a)
 #define IF(cond, t, f) (cond ? t : f)
-template <class T>
-void OFILE(T finp, T fout)
-{
-    freopen(finp, "r", stdin), freopen(fout, "w", stdout);
-}
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
 
 void solve()
 {
     // Solution
 }
 
-sint main()
+signed main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie();
+    FAST_IO;
 
     solve();
 
