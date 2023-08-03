cb57c624bbd8d61d5606b4f491211e02de094e57
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-08-03 13:42:52 +0700

    4605. CKPRIME1 - CHECK PRIME 1

---
+++ 4605_CKPRIME1.cpp
@@ -0,0 +1,52 @@
+//* 810738	03/08/2023 10:07:16	Minh4893IT	CKPRIME1 - CHECK PRIME 1	GNU C++20	Accepted	625 ms	2584 KB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ss stringstream
+#define uset unordered_set
+#define umap unordered_map
+#define mmap multimap
+#define mset multiset
+#define pq priority_queue
+#define endl "\n"
+
+#define LLMAX INT64_MAX
+#define GET(var) getline(cin, var)
+#define EACH(x, a) for (auto &x : a)
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), cin.tie()
+
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+    if (n == 2 || n == 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i += 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    cout << IF(isPrime(n), "YES", "NO");
+}
+
+signed main()
+{
+    FAST_IO;
+
+    solve();
+
+    return 0;
+}
